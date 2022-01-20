6bd16b82ded1a10cc131f5be3bb3f128cbdec8f2
MarcosPaulo15 <marcos.machado20@outlook.com>
2022-01-20 10:03:10 -0300

    Created person class

---
+++ DigioneBank/app/src/main/java/one_digital_innovation/digonebank/Pessoa.kt
@@ -0,0 +1,14 @@
+package one_digital_innovation.digonebank
+
+class Pessoa {
+
+    var nome: String = "Marcos"
+    var cpf: String = "221.456.784-14"
+
+}
+
+fun Main(){
+    val Marcos = Pessoa()
+    print(Marcos.nome)
+    print(Marcos.cpf)
+}
